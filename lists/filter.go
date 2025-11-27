cf49d4e2be4b0afab8c1eaa3e177c88e1533da61
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-11-27 11:44:03 +0100

    filterlessgreater

--- lists/filter.go
+++ lists/filter.go
@@ -4,8 +4,14 @@ package lists
 // Verwenden Sie Rekursion und benutzen Sie NICHT die len-Funktion.
 // Sie können die Hilfsfunktion Empty aus empty.go verwenden.
 func FilterLess(list []int, key int) []int {
-	// TODO
-	return []int{}
+	if Empty(list) {
+		return list
+	}
+	if list[0] > key {
+		return FilterLess(list[1:], key)
+	}
+
+	return []int{}bjhvb
 }
 
 // Liefert eine Liste mit allen Elementen aus list, die echt größer als key sind.
@@ -13,7 +19,12 @@ func FilterLess(list []int, key int) []int {
 // Sie können die Hilfsfunktion Empty aus empty.go verwenden.
 func FilterGreater(list []int, key int) []int {
 	// Gehen Sie analog zu FilterLess vor.
+	if Empty(list) {
+		return list
+	}
+	if list[0] <= key {
+		return FilterGreater(list[1:], key)
+	}
 
-	// TODO
-	return []int{}
+	return []int{}bjhvb
 }
